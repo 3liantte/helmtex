3e937efb8ecc1df2b2ff5461db675285bb02433b
3liantte <eliantte49@gmail.com>
2024-11-12 12:23:02 +0200

    welcome home update

--- components/HomePage/Welcomepage.js
+++ components/HomePage/Welcomepage.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import Image from 'next/image';
+import backgroundImage from "@/public/assets/background2.jpg"
 const WelcomePage = () => {
   return (
-    <div className="relative h-screen bg-cover bg-center sm:bg-right md:bg-center lg:bg-cover" style={{ backgroundImage: "url('/assets/background2.jpg')", priority:"true", quality:"100" }}>
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+    <div className="relative h-screen bg-cover">
+      <Image
+        src={backgroundImage}
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        priority={true}
+        className='bg-center sm:bg-right md:bg-center lg-bg-cover'
+      />
+      <div className="absolute inset-0 bg-black opacity-60"></div>
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4 sm:px-8">
         <h1 className="text-md sm:text-lg md:text-2xl font-light uppercase tracking-wider animate-fade-in-up">Welcome to</h1>
         <h2 className="text-5xl sm:text-5xl md:text-7xl font-bold my-2 sm:my-4 animate-fade-in-up-delayed">Helm Textile Mills</h2>
